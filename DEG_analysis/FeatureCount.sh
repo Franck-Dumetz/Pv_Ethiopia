4b3d192279d65def772f6bbba97d850921d8f1bc
Franck-Dumetz <105318680+Franck-Dumetz@users.noreply.github.com>
2025-01-24 15:33:04 -0500

    Update FeatureCount.sh

--- DEG_analysis/FeatureCount.sh
+++ DEG_analysis/FeatureCount.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-
+#SBATCH --job-name=featureCounts                      # Job name
+#SBATCH --output=/local/projects-t3/SerreDLab-3/fdumetz/Pv_ethiopia/featureCounts.out   # Standard output log
+#SBATCH --error=/local/projects-t3/SerreDLab-3/fdumetz/Pv_ethiopia/featureCounts.err    # Standard error log
+#SBATCH --mail-type=BEGIN,END --mail-user=fdumetz@som.umaryland.edu
+#SBATCH --cpus-per-task=32				# Number of CPUs per task
+#SBATCH --mem=30G                                        # Memory per node
+#SBATCH --partition=all
 # Using featureCount to generate a count table
 
 # Paths to resources
 ANNOTATION="/local/projects-t3/SerreDLab-3/fdumetz/Pv_ethiopia/PlasmoDB-68_PvivaxP01.gff"
-OUTPUT="Pv_Ethiopia_PvAligned.txt"
-THREADS=8
+OUTPUT="Pv_Ethiopia_PvCount.txt"
+THREADS=32
 
 # Collect all BAM files from multiple directories
-BAM_FILES=$(find /local/projects-t3/SerreDLab-3/fdumetz/Pv_ethiopia/bam_Pv_aligned/bam_dupRM -name "*.bam")
+BAM_FILES=$(find /local/projects-t3/SerreDLab-3/fdumetz/Pv_ethiopia/bam_Pv2P01_dedup -name "*.bam")
 
 # Run featureCounts
 featureCounts -a $ANNOTATION \
@@ -16,4 +22,8 @@ featureCounts -a $ANNOTATION \
               -t exon \
               -g gene_id \
               -T $THREADS \
+              -p \
+              -s 2 \
+              -C --largestOverlap \
+              -M --primary \
               $BAM_FILES
